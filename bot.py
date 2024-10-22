7b02070ba8fab2648c5498c9bab3ce73d0e3ab54
paradaise <paradaiseru@gmail.com>
2024-10-22 18:22:36 +0300

    fix

--- bot.py
+++ bot.py
@@ -3,12 +3,13 @@ from api_key import TOKEN
 from text_to_speech import convert_text_to_speech
 from speech_to_text import speech_to_text
 
-bot = telebot.TeleBot(TOKEN)
+bot = telebot.TeleBot(TOKEN)    
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.send_message(message.chat.id, f"Привет,<b>{message.from_user.username}</b>🤤\nЕсли отправишь мне ГС я переведу его в текст!😱\nА если отправишь мне текст я переведу его в ГС!😎",parse_mode="html")
-    bot.send_message(message.chat.id, f"И да, ты можешь пересылать и текст и аудио.",parse_mode="html")
+    bot.send_message(message.chat.id, f"И да, ты можешь пересылать как текст, так и голосовое сообщение.",parse_mode="html")
+    bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAEJYDZnF7KdbNKdQQ4UyWmnNXBcQ-ShfwACHj8AAgnoEEgB8nsNObsWRjYE')
 
 @bot.message_handler(content_types=['text','voice'])
 def handle_message(message):
@@ -17,12 +18,20 @@ def handle_message(message):
     if message.content_type == 'voice':
         speech_to_text(bot,message)
 
+@bot.message_handler(content_types = ['photo', 'sticker', 'location','document'])
+def handle_error(message):
+    bot.send_message(message.chat.id, f"Извини, бот не работает с <i>{message.content_type}!</i> ",parse_mode="html")
+
+@bot.message_handler(content_types = ['video', 'video_note', 'audio'])
+def handle_error(message):
+    bot.send_message(message.chat.id, f"Перевод <i>{message.content_type}</i> в текст пока находится в разработке:((",parse_mode="html")
+
 
 bot.polling(non_stop=True)
 
 
+
 #добавить в бота:
-    #сообщение что бот работает только с текстом и голосом если пришло геолокация,стикер
     #переводить в текст песни,видео,кружки
-    #добавить автоопределения text to speech, если пришло сообщение на английском подставлять в параметр lang="eu" 
+    #добавить автоопределения TTS, если пришло сообщение на английском подставлять в параметр lang="eu" 
     #добавить выбор голоса для text-to-speech(мужчина,женщина)
--- text_to_speech.py
+++ text_to_speech.py
@@ -1,10 +1,14 @@
 import gtts
 
 def convert_text_to_speech(bot,message):
-    tts = gtts.gTTS(message.text, lang="ru")
-    tts_path = f"text/{message.from_user.username}.wav"
-    tts.save(tts_path)
-    bot.send_message(message.chat.id, f"<i>{message.from_user.username}</i>,вот твое голосовое сообщение!🗣",parse_mode="html")
-    bot.send_audio(message.chat.id, open(tts_path,'rb'))
+    if all(char in ['!', '.', ',', ')', '('] for char in message.text.strip()):
+        bot.send_message(message.chat.id, f"<i>{message.from_user.username}</i>, не отправляй эти симоволы:['!','.',',',')','('] без текста",parse_mode="html")
+        return
+    else:
+        tts = gtts.gTTS(message.text, lang="ru")
+        tts_path = f"text/{message.from_user.username}.wav"
+        tts.save(tts_path)
+        bot.send_message(message.chat.id, f"<i>{message.from_user.username}</i>,вот твое голосовое сообщение!🗣",parse_mode="html")
+        bot.send_audio(message.chat.id, open(tts_path,'rb'))
 
 
